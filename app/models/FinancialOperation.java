5bf9bc794612970580763571399cbce58f5955b0
Dan Costelloe <danc@macfeegle.local>
2013-02-24 13:34:22 +0000

    added basic lodgement and acc summary pages and behaviour

---
+++ app/models/FinancialOperation.java
@@ -0,0 +1,14 @@
+package models;
+
+/**
+ * Represents financial operations (e.g. lodgements, withdrawals)
+ * @author danc
+ */
+public class FinancialOperation {
+
+	public String type;
+	
+	public String accountNumber;
+	
+	public Double amount;
+}
